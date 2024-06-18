31a96c01821516a2ee74c7ab2adce3d716e2f0d5
shivaph <shivaph@hotmail.com>
2024-06-18 14:32:50 -0700

    commit

---
+++ mysite/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
+
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
